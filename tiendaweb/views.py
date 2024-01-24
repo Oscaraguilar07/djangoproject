19da6dda202bb4fff5cc2462fb3d4c9316b1e5c4
wilson2106-chacón <wilsonestebanchaconcontreras@gmail.com>
2024-01-24 10:58:40 -0500

    login cambios

--- tiendaweb/views.py
+++ tiendaweb/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.shortcuts import redirect
 from django.http import HttpResponse
+from django.contrib import messages
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate
 
 def index (request):
     return render (request,'index.html',{
@@ -8,13 +11,24 @@ def index (request):
     })
 
 def login_views (request):
+    if request.method == 'POST':
+        usuario = request.POST.get('usario')
+        contraseña = request.POST.get('contraseña')
+        
+        user = authenticate(username=usuario, contraseña=contraseña)
+        if user:
+            login(request, user)
+            messages.success(request, 'Bienvenido {}'.format(user.username))
+            return redirect ('index')
+        else
+        messages.error(request, 'usuario o contraseña no vaido')
+        
     return render (request,'login.html', {
  
-    })
+    })  
     
 def registro_views (request):
     if request.method == 'POST':
-        
         usuario = request.POST.get('usuario')
         correo = request.POST.get('correo')
         password_plano = request.POST.get('clave')
